6910acfec5ff490839f349bd116c4f30a0d47491
Nayak <anuja.nayak@capgemini.com>
2023-04-13 17:26:58 +0530

    bhai data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const EmployeeExperienceReport = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pdf-container" id="pdfdata"
+      style={{ padding: '15px', fontFamily: 'Times New Roman,Times,serif' }}>
+
+      <div style={{ marginBottom: '10px' }}>&nbsp;</div>
+      <table width="100%">
+        <tbody>
+          <tr>
+            <td colSpan="6">&nbsp;</td>
+          </tr>
+          <tr>
+            <td colSpan="6">
+              <h2 style={{ fontSize: '26px' }}>
+                Aarvee Associates Architects Engineers &amp; Consultants Pvt Ltd
+              </h2>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="6">
+              Ravula Residency Srinagar Colony Main Road Hyderabad Telangana India
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="6">&nbsp;</td>
+          </tr>
+          <tr>
+            <td colSpan="6">
+              <h3>Employee Experience Report</h3>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="6">
+              <hr />
+            </td>
+          </tr>
+          <tr>
+            <td width="20%">Employee Code :</td>
+            <td width="80%" colSpan="5">
+              7776
+            </td>
+          </tr>
+          <tr>
+            <td>Employee Name :</td>
+            <td>Amit Nayak</td>
+          </tr>
+          <tr>
+            <td> Company Designation :</td>
+            <td>Assistant Manager</td>
+          </tr>
+          <tr>
+            <td>Project Designation:</td>
+            <td>Assistant Resident Engineer</td>
+          </tr>
+          <tr>
+            <td>Service Period:</td>
+            <td>17. Aug.2020 To 19. Sep.2021</td>
+          </tr>
+          <tr>
+            <td>Date of Relieving :</td>
+            <td>19. Sep.2021</td>
+          </tr>
+          <tr>
+            <td> Certificate Date:</td>
+            <td>21. Jan.2022</td>
+          </tr>
+          <tr>
+            <td>Certifying Authority:</td>
+            <td>B. Venkateshwar Reddy</td>
+          </tr>
+          <tr>
+            <td colSpan="6">
+              <hr />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
-export default App;
+export default EmployeeExperienceReport;
